666397dec317f96c0cd0a7b1bc3ccbf6502bafb4
heitorgusan <heitorgusan@gmail.com>
2022-05-08 19:13:15 -0300

    Venda Controller

--- porto-tech-store/src/main/java/com/portotechstore/portotechstore/controller/VendaController.java
+++ porto-tech-store/src/main/java/com/portotechstore/portotechstore/controller/VendaController.java
@@ -1,5 +1,56 @@
 package com.portotechstore.portotechstore.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.portotechstore.portotechstore.model.ClienteModel;
+import com.portotechstore.portotechstore.model.VendaModel;
+import com.portotechstore.portotechstore.repository.VendaRepository;
+
+@RestController
+@RequestMapping("/vendas")
+@CrossOrigin("*")
 public class VendaController {
 
+	@Autowired
+	VendaRepository vendaRepository;
+	
+	@GetMapping
+	public ResponseEntity<List<VendaModel>>GetAll(){
+		return ResponseEntity.ok(vendaRepository.findAll());
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<VendaModel>GetByIdCliente(@PathVariable long id){
+		return vendaRepository.findById(id)
+				.map(resp -> ResponseEntity.ok(resp))
+				.orElse(ResponseEntity.notFound().build());
+	}
+	
+	@PostMapping()
+	public ResponseEntity<VendaModel>post(@RequestBody VendaModel venda){
+		return ResponseEntity.status(HttpStatus.CREATED).body(vendaRepository.save(venda));
+	}
+	
+	@PutMapping
+	public ResponseEntity<VendaModel> put (@RequestBody VendaModel venda){
+		return ResponseEntity.status(HttpStatus.OK).body(vendaRepository.save(venda));
+	}
+	
+	@DeleteMapping("/{id}")
+	public void delete(@PathVariable long id) {
+		vendaRepository.deleteById(id);
+	}	
 }
