49bdf7fccc68da3d4b41f1e38a0457db6907409b
heitorgusan <heitorgusan@gmail.com>
2022-06-20 14:45:04 -0300

    tirando o security

--- porto-tech-store/src/main/java/com/portotechstore/portotechstore/security/BasicSecurityConfig.java
+++
@@ -1,8 +0,0 @@
-package com.portotechstore.portotechstore.security;
-
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-
-@EnableWebSecurity //Annotation que irá habilitar a configurção para web security;
-public class BasicSecurityConfig {
-
-}
